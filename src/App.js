ba9920725239d22c2cfa68fa0b70dc82481c234c
Ashwin-Iyer1 <72718544+Ashwin-Iyer1@users.noreply.github.com>
2024-04-08 23:05:15 -0500

    add page view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes   } from "react-router-dom";
 import "./App.css";
 import About from "./About";
 import Projects from "./Projects";
 import Home from "./Home";
+import ReactGA from 'react-ga4';
+ReactGA.initialize("G-T57ZFGY2HZ");
+  
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview" , page: window.location.pathname + window.location.search});
+  }, []);
 
   return (
     <div className="App">
